20ec5523a1e9b0e87ebca25e863c112f7606f084
Daniel <danieloftavares@gmail.com>
2024-12-14 09:52:21 -0300

    Shape rotation and changing colors

--- Animation2/2.cpp
+++ Animation2/2.cpp
@@ -5,6 +5,7 @@ int main(){
 
 	int default_width = 1000;
 	int default_height= 1000;
+	float angle = 0.0;
 
 	sf::ContextSettings settings;
 	settings.antialiasingLevel=8;
@@ -13,13 +14,13 @@ int main(){
 	window.setFramerateLimit(60);
 
 	sf::CircleShape shape(300.f);
+
 	shape.setOutlineThickness(2);
-	shape.setOutlineColor(sf::Color::Cyan);
 	shape.setFillColor(sf::Color::Transparent);
 	shape.setPointCount(300);
 	shape.setOrigin(300.f, 300.f);
 	shape.setPosition(default_width/2, default_height/2);
-	
+
 	while(window.isOpen()){
 		sf::Event event;
 		while (window.pollEvent(event)){
@@ -27,6 +28,16 @@ int main(){
 				window.close();
 			}
 		}
+		int blueColor= (blueColor + 1) % 256;
+		int redColor = (redColor + 2) % 256;
+		int greenColor = (greenColor + 3) % 256;
+		angle += 0.1;
+		if (angle >= 360.0){
+			angle = 0.0;}
+
+		shape.setRotation(angle);
+		shape.setOutlineColor(sf::Color(redColor, greenColor, blueColor));
+
 		window.clear();
 		window.draw(shape);
 		window.display();
